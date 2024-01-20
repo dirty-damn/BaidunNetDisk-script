e88eeb2ef0125a4b5b80867b931e1fac3783bd82
liong.liu <sensei.liu@qq.com>
2024-01-20 18:51:59 +0800

    重构

--- 百度网盘_文件库清单导出.user.js
+++ 百度网盘_文件库清单导出.user.js
@@ -16,154 +16,225 @@
     'use strict'
     // Your code here...
 
-    var fileInfo = ''
-    var startTime
-    var checkUrl = []
-    var statistic = 0
-    var size = 0
-    var pageSize = 500
-    function init() {
-        fileInfo = ''
-        startTime = new Date()
-        checkUrl = []
-        statistic = 0
-    }
-
-    function exportDir() {
-        init()
-
-        let selectedList = $("tr[class='im-pan-table__body-row mouse-choose-item selected']")
-        for (let i = 0; i < selectedList.length; i++) {
-            let selected = selectedList[i];
-            let fs_id = selected.dataset.id
-            query(fs_id, 1)
-        }
-
-        let gb = size / 1024 / 1024 / 1024
-        let mb = size / 1024 / 1024
-        let result = `总耗时：${new Date().getTime() - startTime.getTime()}ms\n`
-            + `共导出资源数：${statistic}\n`
-            + `共计资源大小：${gb.toFixed(2)}GB = ${mb.toFixed(2)}MB`
-        console.log(result)
-        fileInfo = result + '\n\n' + fileInfo
-        var blob = new Blob([fileInfo], {type: "text/plaincharset=utf-8"})
-        saveAs(blob, "exportDirList.txt")
-    }
-
-    function query(fs_id, pageNo) {
-        let gid_url = `https://pan.baidu.com/mbox/msg/shareinfo?page=${pageNo}&num=${pageSize}&fs_id=${fs_id}&from_uk=${from_uk}&msg_id=${msg_id}&type=2&gid=${gid}&limit=50&desc=1&clienttype=0&app_id=250528&web=1&dp-logid=${dp_logid}`
-        if (checkUrl.indexOf(gid_url) > -1) {
-            console.log(`请求重复：${gid_url}`)
-            throw new Error(`请求重复：${gid_url}`)
-        }
-        $.ajax({
-            type:'GET',
-            url: gid_url,
-            data:{},
-            dataType: "json",
-            async: false,
-            success: (res) => {
-                console.log(`url：`, gid_url)
-                // console.log(`返回结果：`, res)
-                if (res.errno != 0) {
-                    console.error('获取异常', res)
-                }
-                let hasMore = res.has_more
-                addFileInfo(res.records)
-                if (hasMore === 1) {
-                    query(fs_id, pageNo + 1)
-                }
+    const Mode = {
+        FILE: 1,
+        EXCEL: 2
+    };
+    
+    const config = {
+        // 请求分页每页500条
+        pageSize: 500,
+        // 每500条打印一次日志
+        tipsCount: 500,
+        
+        exportMode: {
+            mode: Mode.FILE,
+            file: {
+                SPLITTER: ', '
             },
-            error:function(err){
-                console.error(err)
-            }
-        })
+            excel: {}
+        }
     }
+    
+    var exportInfo = {
+        content: [],
+        statistic: {
+            fileCount: 0,
+            fileSize: 0,
+            startTime: new Date(),
+        },
+        
+        exportSelectedDir: function() {
+            exportInfo.init()
+            exportInfo.exportEveryDir()
+            exportInfo.save()
+        },
+        init: function() {
+            this.content = []
 
-    // 定义分隔符
-    var SPLITTER = ', '
-    var tipsCount = 500
-    function addFileInfo(records) {
-        if (records == null || records.length == 0) {
+            this.statistic.fileCount = 0
+            this.statistic.fileSize = 0
+            this.statistic.startTime = new Date()
+        },
+        exportEveryDir: function() {
+            let selectedList = $("tr[class='im-pan-table__body-row mouse-choose-item selected']")
+            for (let i = 0; i < selectedList.length; i++) {
+                let selected = selectedList[i];
+                let fsId = selected.dataset.id
+                this.loopQueryPage(fsId, 1)
+            }
+        },
+        loopQueryPage: function (fsId, pageNo) {
+            let gidUrl = `https://pan.baidu.com/mbox/msg/shareinfo?page=${pageNo}&num=${config.pageSize}&fs_id=${fsId}&from_uk=${authInfo.fromUk}&msg_id=${authInfo.msgId}&type=2&gid=${authInfo.gid}&limit=50&desc=1&clienttype=0&app_id=250528&web=1&dp-logid=${authInfo.dpLogid}`
+            $.ajax({
+                type:'GET',
+                url: gidUrl,
+                data:{},
+                dataType: "json",
+                async: false,
+                success: (res) => {
+                    console.log(`url：`, gidUrl)
+                    if (res.errno != 0) {
+                        console.error('获取异常', res)
+                    }
+                    let hasMore = res.has_more
+                    this.processBiz(res.records)
+                    if (hasMore === 1) {
+                        this.loopQueryPage(fsId, pageNo + 1)
+                    }
+                },
+                error:function(err){
+                    console.error(err)
+                }
+            })
+        },
+        processBiz: function(records) {
+            if (records == null || records.length == 0) {
+                return
+            }
+
+            for (let i = 0; i < records.length; i++) {
+                let record = records[i]
+                if (record.isdir == 0) {
+                   this.doBiz(record)
+                } else {
+                    this.loopQueryPage(record.fs_id, 1)
+                }
+            }
+        },
+        doBiz: function(record) {
+            let fileMb = record.size / 1024 / 1024
+            let fileObj = {
+                name: record.server_filename,
+                path: record.path,
+                size: fileMb.toFixed(2),
+                fsId: record.fs_id,
+                localMtime: record.local_mtime,
+                serverMtime: record.server_mtime
+            }
+            this.content.push(fileObj)
+
+            // 计数
+            this.recordStatistic(record)
+        },
+        recordStatistic: function(record) {
+            this.statistic.fileCount++
+            this.statistic.fileSize += record.size
+            if (this.statistic.fileCount % config.tipsCount === 0) {
+                console.log(`累计导出资源数：${this.statistic.fileCount}`)
+                let gb = this.statistic.fileSize / 1024 / 1024 / 1024
+                let mb = this.statistic.fileSize / 1024 / 1024
+                console.log(`累计资源大小：${gb.toFixed(2)}GB = ${mb.toFixed(2)}MB`)
+                let time = new Date().getTime() - this.statistic.startTime.getTime();
+                console.log(`累计耗时：${time}ms，开始时间：${this.statistic.startTime}`)
+            }
+        },
+        save: function() {
+            // 打印执行结果
+            let result = this.printResult()
+            
+            if (config.exportMode.mode === Mode.FILE) {
+                this.writeToFile(result)
+            } 
+            
+            if (config.exportMode.mode === Mode.FILE)  {
+                this.writeToExcel(result)
+            }
+        },
+        printResult: function() {
+            let gb = this.statistic.fileSize / 1024 / 1024 / 1024
+            let mb = this.statistic.fileSize / 1024 / 1024
+            let result = `总耗时：${new Date().getTime() - this.statistic.startTime.getTime()}ms\n`
+                + `共导出资源数：${this.statistic.statistic}\n`
+                + `共计资源大小：${gb.toFixed(2)}GB = ${mb.toFixed(2)}MB`
+            console.log(result)
+
+            return result
+        },
+        writeToFile: function(result) {
+            let contentStr = result + '\n\n'
+            for (let i = 0; i < this.content.length; i++) {
+                let fileObj = this.content[i]
+                // 资源名称, 路径, 大小MB，网盘资源的fs_id
+                contentStr += fileObj.name + config.exportMode.file.SPLITTER + fileObj.path + config.exportMode.file.SPLITTER + fileObj.size + config.exportMode.file.SPLITTER + fileObj.fsId + '\n'
+            }
+            
+            let blob = new Blob([contentStr], {type: "text/plaincharset=utf-8"})
+            saveAs(blob, "exportDirList.txt")
+        },
+        writeToExcel: function(result) {
+            // TODO: 导出excel
+        },
+    }
+    
+    let authInfo = {
+        gid: '',
+        fromUk: '',
+        msgId: '',
+        dpLogid: '',
+        isChangeFileFolder: false,
+        
+        init: function() {
+            this.initAuthInfo()
+            this.addExportButton()
+        },
+        initAuthInfo: function() {
+            let _this = this
+            const originOpen = XMLHttpRequest.prototype.open
+            XMLHttpRequest.prototype.open = function (_, url) {
+                _this.clickGroupFileLibrary(url)
+                _this.clickOneFileFolder(url)
+                originOpen.apply(this, arguments)
+            }
+        },
+        addExportButton: function() {
+            let $dropdownbutton = $('<button type="button" class="u-button u-button--default u-button--mini"><span>导出</span></button>')
+            $dropdownbutton.click(exportInfo.exportSelectedDir)
+            var task = setInterval(() => {
+                var buttons = $("div[class='im-file-nav__operate']")
+                if (buttons.text().indexOf('导出') < 0) {
+                    buttons.append($dropdownbutton)
+                }
+            }, 2000)
+        },
+        clickGroupFileLibrary: function(url) {
+            if (url.indexOf("/mbox/group/listshare") >= 0) {
+                let gidNew = this.getUrlParamValueByName(url, 'gid')
+                if (authInfo.gid != gidNew) {
+                    authInfo.isChangeFileFolder = true
+                    authInfo.gid = gidNew
+                    console.log('准备进度[1/4]，拿到[gid]啦，玲宝O(∩_∩)O哈哈~：' + authInfo.gid)
+                }
+            }
+        },
+        clickOneFileFolder: function(url) {
+            if (url.indexOf("/mbox/msg/shareinfo") >= 0 && authInfo.fromUk.length <= 0) {
+                authInfo.fromUk = this.getUrlParamValueByName(url, 'from_uk')
+                console.log('准备进度[2/4]，拿到[from_uk]啦，玲宝O(∩_∩)O哈哈~：' + authInfo.fromUk)
+            }
+
+            if (url.indexOf("/mbox/msg/shareinfo") >= 0 && authInfo.isChangeFileFolder) {
+                authInfo.msgId = this.getUrlParamValueByName(url, 'msg_id')
+                console.log('准备进度[3/4]，拿到[msg_id]啦，玲宝O(∩_∩)O哈哈~：' +authInfo. msgId)
+                authInfo.dpLogid = this.getUrlParamValueByName(url, 'dp-logid')
+                console.log('准备进度[4/4]，拿到[dp_logid]啦，玲宝O(∩_∩)O哈哈~：' + authInfo.dpLogid)
+                console.log('准备完成，可以开始导出啦，玲宝O(∩_∩)O哈哈~')
+                authInfo.isChangeFileFolder = false
+            }
+        },
+        getUrlParamValueByName: function(url, name) {
+            // \b 边界
+            // ?<= 向后匹配
+            // 字符串转成正则表达式，其中的'\b'类型的特殊字符要多加一个'\'
+            let reg = new RegExp(`(?<=\\b${name}=)[^&]*`)
+            let target = url.match(reg)
+
+            if(target) {
+                return target[0]
+            }
+
             return
         }
-
-        for (let i = 0; i < records.length; i++) {
-            let record = records[i]
-            if (record.isdir == 0) {
-                let mb = record.size / 1024 / 1024
-                fileInfo += record.server_filename + SPLITTER + record.path + SPLITTER + mb.toFixed(2) + SPLITTER + record.fs_id + '\n'
-                statistic++
-                size += record.size
-                if (statistic % tipsCount === 0) {
-                    console.log(`累计导出资源数：${statistic}`)
-                    let gb = size / 1024 / 1024 / 1024
-                    let mb = size / 1024 / 1024
-                    console.log(`累计资源大小：${gb.toFixed(2)}GB = ${mb.toFixed(2)}MB`)
-                    let time = new Date().getTime() - startTime.getTime();
-                    console.log(`累计耗时：${time}ms，开始时间：${startTime}`)
-                }
-            } else {
-                query(record.fs_id, 1)
-            }
-        }
     }
-
-    function getUrlParamsByName(str, name) {
-        // \b 边界
-        // ?<= 向后匹配
-        // 字符串转成正则表达式，其中的'\b'类型的特殊字符要多加一个'\'
-        let reg = new RegExp(`(?<=\\b${name}=)[^&]*`)
-        let target = str.match(reg)
-
-        if(target) {
-            return target[0]
-        }
-
-        return
-    }
-
-    var gid = ''
-    var changeFlag = false
-    var from_uk = ''
-    var msg_id = ''
-    var dp_logid = ''
-    const originOpen = XMLHttpRequest.prototype.open
-    XMLHttpRequest.prototype.open = function (_, url) {
-        if (url.indexOf("/mbox/group/listshare") >= 0) {
-            console.log(url)
-            let gidNew = getUrlParamsByName(url, 'gid')
-            if (gid != gidNew) {
-                changeFlag = true
-                gid = gidNew
-                console.log('拿到[gid]啦，玲宝O(∩_∩)O哈哈~：' + gid)
-            }
-        }
-        if (url.indexOf("/mbox/msg/shareinfo") >= 0 && from_uk.length <= 0) {
-            console.log(url)
-            from_uk = getUrlParamsByName(url, 'from_uk')
-            console.log('拿到[from_uk]啦，玲宝O(∩_∩)O哈哈~：' + from_uk)
-        }
-        if (url.indexOf("/mbox/msg/shareinfo") >= 0 && changeFlag) {
-            console.log(url)
-            msg_id = getUrlParamsByName(url, 'msg_id')
-            console.log('拿到[msg_id]啦，玲宝O(∩_∩)O哈哈~：' + msg_id)
-            dp_logid = getUrlParamsByName(url, 'dp-logid')
-            console.log('拿到[dp_logid]啦，玲宝O(∩_∩)O哈哈~：' + dp_logid)
-            console.log('准备就绪啦，可以开始导出啦，玲宝O(∩_∩)O哈哈~')
-            changeFlag = false
-        }
-
-        originOpen.apply(this, arguments)
-    }
-
-    var $dropdownbutton = $('<button type="button" class="u-button u-button--default u-button--mini"><span>导出</span></button>')
-    $dropdownbutton.click(exportDir)
-    var task = setInterval(() => {
-        var buttons = $("div[class='im-file-nav__operate']")
-        if (buttons.text().indexOf('导出') < 0) {
-            buttons.append($dropdownbutton)
-        }
-    }, 2000)
-
+    authInfo.init()
 })()
